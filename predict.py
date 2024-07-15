5a77053d095780ba8f3270f3446a4989770cff3d
yimo <skylo.li@qq.com>
2024-07-15 17:54:54 +0800

    add GUI for predict

--- predict.py
+++ predict.py
@@ -1,8 +1,8 @@
-import pandas as pd 
+import pandas as pd
 import numpy as np
 from utils.DataLoade import CustomDataset
 # from torch.utils.data import DataLoader
-from model.FCN import FCN32s,FCN8x
+from model.FCN import FCN32s, FCN8x
 from model.Unet import UNet
 import torch
 import os
@@ -10,6 +10,7 @@ from model.DeepLab import DeepLabV3
 # from torch import nn,optim
 # from torch.nn import functional as F
 from utils.eval_tool import label_accuracy_score
+import window
 
 # model = 'UNet'
 model = 'FCN8x'
@@ -20,34 +21,36 @@ INPUT_HEIGHT = 320
 BATCH_SIZE = 32
 NUM_CLASSES = 21
 LEARNING_RATE = 1e-3
-model_path='./model_result/best_model_{}.mdl'.format(model)
+model_path = './model_result/best_model_{}.mdl'.format(model)
 # model_path='./model_result/best_model_{}_kaggle.mdl'.format(model)
 
 torch.cuda.set_device(GPU_ID)
 net = FCN8x(NUM_CLASSES)
+
+
 # net = DeepLabV3(NUM_CLASSES)
 # net = UNet(3,NUM_CLASSES)
-#加载网络进行测试
+# 加载网络进行测试
 def evaluate(model):
-    import random 
-    from utils.DataLoade import label2image,RandomCrop
+    import random
+    from utils.DataLoade import label2image, RandomCrop
     import matplotlib.pyplot as plt
     from torch.utils.data import DataLoader
     from PIL import Image
 
     test_csv_dir = 'test.csv'
-    testset = CustomDataset(test_csv_dir,INPUT_WIDTH,INPUT_HEIGHT)
-    test_dataloader = DataLoader(testset,batch_size = 15,shuffle=False)
+    testset = CustomDataset(test_csv_dir, INPUT_WIDTH, INPUT_HEIGHT)
+    test_dataloader = DataLoader(testset, batch_size=15, shuffle=False)
 
-    net.load_state_dict(torch.load(model_path,map_location='cuda'))
+    net.load_state_dict(torch.load(model_path, map_location='cuda'))
     # index = random.randint(0, len(testset) - 1)
     # index = [5,6]
-    for (val_image,val_label) in test_dataloader:
-    # val_image, val_label = test_dataloader[1]
+    for (val_image, val_label) in test_dataloader:
+        # val_image, val_label = test_dataloader[1]
         net.cuda()
-        out = net(val_image.cuda())  #[10, 21, 320, 320]
+        out = net(val_image.cuda())  # [10, 21, 320, 320]
         pred = out.argmax(dim=1).squeeze().data.cpu().numpy()  # [10,320,320]
-        label = val_label.data.numpy() # [10,320,320]
+        label = val_label.data.numpy()  # [10,320,320]
         val_pred, val_label = label2image(NUM_CLASSES)(pred, label)
 
         for i in range(15):
@@ -59,22 +62,20 @@ def evaluate(model):
             std = [.229, .224, .225]
             x = val_imag
             for j in range(3):
-                
-                x[j]=x[j].mul(std[j])+mean[j]
+                x[j] = x[j].mul(std[j]) + mean[j]
             img = x.mul(255).byte()
-            img = img.numpy().transpose((1, 2, 0)) # 原图
+            img = img.numpy().transpose((1, 2, 0))  # 原图
 
-
-
-
-            fig, ax = plt.subplots(1, 3,figsize=(30,30))
+            fig, ax = plt.subplots(1, 3, figsize=(30, 30))
             ax[0].imshow(img)
             ax[1].imshow(val_labe)
             ax[2].imshow(val_pre)
             # plt.show()
-            plt.savefig('./pic_results/pic_{}_{}.png'.format(model,i))
+            plt.savefig('./pic_results/pic_{}_{}.png'.format(model, i))
         break  # 只显示一个batch
 
-if __name__ == "__main__":
 
+if __name__ == "__main__":
     evaluate(model)
+    app = window.ImageSwitcher()
+    app.mainloop()
